75da2f18d0a9c304a4325a4ba23cbe0893e5d090
rijalislamir <rijalislamir@gmail.com>
2022-01-19 00:46:21 +0700

    Rename postgres to be postgresql

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -5,7 +5,7 @@ from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bcyibotyscmphi:630df1b531c1f7fdce1d59ff43ce27d84a5339bac3f6849070175856707b4ed3@ec2-3-222-49-168.compute-1.amazonaws.com:5432/daitfpr32kdg57'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bcyibotyscmphi:630df1b531c1f7fdce1d59ff43ce27d84a5339bac3f6849070175856707b4ed3@ec2-3-222-49-168.compute-1.amazonaws.com:5432/daitfpr32kdg57'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
